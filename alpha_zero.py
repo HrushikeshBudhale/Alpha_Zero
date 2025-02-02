63f750cebf0b2099630c1cfae2bd0ea2716ccd8d
Hrushikesh <hruhnb@gmail.com>
2025-02-01 20:17:42 -0500

    Tested AlphaZero and training script

--- alpha_zero.py
+++ alpha_zero.py
@@ -3,10 +3,9 @@ import numpy as np
 import random
 import torch.nn.functional as F
 from tqdm import tqdm
-from model import ResNet
 
 class AlphaMCTS_Node:
-    def __init__(self, game, state, parent=None, action_taken=None, prior=0, args=None):
+    def __init__(self, game, state, parent=None, action_taken=None, prior=0, args=None, visit_count=0):
         self.game = game
         self.state = state
         self.parent = parent
@@ -16,7 +15,7 @@ class AlphaMCTS_Node:
         
         self.children = []
         
-        self.visit_count = 0
+        self.visit_count = visit_count
         self.value_sum = 0
         
     def is_fully_expanded(self):
@@ -58,6 +57,7 @@ class AlphaMCTS_Node:
             self.parent.backpropagate(value)
 
 
+
 class AlphaMCTS:
     def __init__(self, game, model, args=None):
         self.game = game
@@ -67,7 +67,16 @@ class AlphaMCTS:
     
     @torch.no_grad()
     def search(self, state):
-        root = AlphaMCTS_Node(self.game, state, args=self.args)
+        root = AlphaMCTS_Node(self.game, state, args=self.args, visit_count=1)
+        
+        encoded_state = torch.tensor(self.game.get_encoded_state(state), device=self.device).unsqueeze(0)
+        policy, value = self.model(encoded_state)
+        policy, value = F.softmax(policy, dim=1).squeeze(0).cpu().numpy(), value.item()
+        policy = (1-self.args["dirichlet_epsilon"]) * policy + \
+                    self.args["dirichlet_epsilon"]  * np.random.dirichlet([self.args["dirichlet_alpha"]]*len(policy))
+        policy *= self.game.get_valid_actions(state) # remove invalid moves
+        policy /= np.sum(policy)
+        root.expand(policy)
         
         for _ in range(self.args["num_searches"]):
             node = root
@@ -77,23 +86,24 @@ class AlphaMCTS:
             
             value, is_terminated = self.game.get_value_and_terminated(node.state, node.action_taken)
             value = self.game.get_opponent_value(value)
+            
             if not is_terminated:
-                valid_actions = self.game.get_valid_actions(node.state)
-                state = torch.tensor(self.game.get_encoded_state(node.state), device=self.device).unsqueeze(0)
-                policy, value = self.model(state)
-                policy = policy.squeeze(0).cpu().numpy()
-                policy *= valid_actions
+                encoded_state = torch.tensor(self.game.get_encoded_state(node.state), device=self.device).unsqueeze(0)
+                policy, value = self.model(encoded_state)
+                policy, value = F.softmax(policy, dim=1).squeeze(0).cpu().numpy(), value.item()
+                policy *= self.game.get_valid_actions(node.state) # remove invalid moves
                 policy /= np.sum(policy)
-                value = value.item()
-                node = node.expand(policy)
+                node.expand(policy)
             
             node.backpropagate(value)
             
         action_probs = np.zeros(self.game.action_size)
         for child in root.children:
             action_probs[child.action_taken] = child.visit_count
-        action_probs = action_probs / np.sum(action_probs)
+        action_probs /= np.sum(action_probs)
         return action_probs
+
+
 
 class AlphaZero:
     def __init__(self, game, model, optim, args=None):
@@ -104,19 +114,33 @@ class AlphaZero:
         self.mcts = AlphaMCTS(game, model, args)
         self.device = args["device"]
         
+    def temperature_based_action(self, action_probs, episode_steps):
+        if episode_steps < self.args["exploration_steps"]:
+            action = np.random.choice(len(action_probs))
+            return action
+        
+        if self.args["temperature"] >= 10: # take random actions
+            action_probs = np.ones(len(action_probs), dtype=np.float32)
+        elif self.args["temperature"] <= 0: # take best action
+            action_probs = np.where(action_probs == np.max(action_probs), 1.0, 0.0)
+        else:
+            action_probs = np.power(action_probs, 1/self.args["temperature"])
+            
+        action_probs /= action_probs.sum()
+        action = np.random.choice(len(action_probs), p=action_probs)
+        return action
+        
     def selfPlay(self):
         memory = []
         state = self.game.get_initial_state()
         player = 1
-        
+        episode_steps = 0
         while True:
             neutral_state = self.game.get_perspective(state, player)
             action_probs = self.mcts.search(neutral_state)
             memory.append((neutral_state, action_probs, player))
-            temperature_action_probs = np.power(action_probs, 1/self.args["temperature"])
-            temperature_action_probs /= np.sum(temperature_action_probs)
-            action = np.random.choice(len(action_probs), p=temperature_action_probs)
-            state = self.game.get_next_state(neutral_state, action, player)
+            action = self.temperature_based_action(action_probs, episode_steps)
+            state = self.game.get_next_state(state, action, player)
             value, is_terminated = self.game.get_value_and_terminated(state, action)
             if is_terminated:
                 valueMemory = []
@@ -124,6 +148,7 @@ class AlphaZero:
                     value_wrt_player = value if player == mem_player else self.game.get_opponent_value(value)
                     valueMemory.append((mem_neutral_state, mem_policy, value_wrt_player))
                 return valueMemory
+            episode_steps += 1
             player = self.game.get_opponent(player)
     
     def train(self, memory):
@@ -162,6 +187,11 @@ class AlphaZero:
             for _ in tqdm(range(self.args["train_iters"]), desc=f"Training"):
                 self.train(memory)
         
-        print("Saving model...")
         torch.save(self.model.state_dict(), self.args["model_path"])
-        torch.save(self.optimizer.state_dict(), self.args["optimizer_path"])    
+        print("Training Complete.")
+        print(f"Model saved to {self.args["model_path"]}")
+    
+    def play(self, state):
+        action_probs = self.mcts.search(state)
+        action = np.argmax(action_probs)
+        return action
